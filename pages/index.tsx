64fd59f5317332943105a28639ad41bf187ef70c
Rafael Freitas da Silva <rfl.fsilva@gmail.com>
2023-03-25 10:06:01 -0300

    Add TS

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,11 @@ import Layout, { siteTitle } from '../components/layout';
 import Link from 'next/link';
 import Date from '../components/date';
 import utilStyles from '../styles/utils.module.css';
+import { GetStaticProps } from 'next'
 
 import { getSortedPostsData } from '../lib/posts';
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
@@ -15,7 +16,13 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData }: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
